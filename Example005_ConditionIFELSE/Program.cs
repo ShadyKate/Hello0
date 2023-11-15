9aff0f24e497e6500d12742c06257bfdfc2d570d
Kate <katehappy16@gmail.com>
2023-11-15 23:05:05 +0300

    Condition

---
+++ Example005_ConditionIFELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "diana")
+{
+    Console.WriteLine("I love you Di");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
